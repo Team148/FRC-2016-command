a6abeb33d0e97f368213d8b7b9435b0984cd06b1
justinGriggs <justin@bustedengineering.com>
2016-06-28 20:54:56 -0500

    Added ActionStop Class

---
+++ src/Subsystems/ActionArm/ActionStop.cpp
@@ -0,0 +1,18 @@
+/*
+ * ActionStop.cpp
+ *
+ *  Created on: Jun 28, 2016
+ *      Author: justin
+ */
+
+#include <Subsystems/ActionArm/ActionStop.h>
+
+ActionStop::ActionStop() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ActionStop::~ActionStop() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Subsystems/ActionArm/ActionStop.h
@@ -0,0 +1,17 @@
+/*
+ * ActionStop.h
+ *
+ *  Created on: Jun 28, 2016
+ *      Author: justin
+ */
+
+#ifndef SRC_SUBSYSTEMS_ACTIONARM_ACTIONSTOP_H_
+#define SRC_SUBSYSTEMS_ACTIONARM_ACTIONSTOP_H_
+
+class ActionStop {
+public:
+	ActionStop();
+	virtual ~ActionStop();
+};
+
+#endif /* SRC_SUBSYSTEMS_ACTIONARM_ACTIONSTOP_H_ */
