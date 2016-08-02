4b89f0d763618da1e51ac3efcf0aa62183117320
#beards <justin@bustedengineering.com>
2016-08-01 19:36:34 -0500

    fixed a syntax error in Ports class.

--- src/Constants/Ports.h
+++ src/Constants/Ports.h
@@ -11,54 +11,55 @@
 
 class Ports {
 
+public:
 	//Drivetrain CAN IDs
-	public const int DRIVE_LEFT_1 = 2;
-	public const int DRIVE_LEFT_2 = 3;
-	public const int DRIVE_RIGHT_1 = 5;
-	public const int DRIVE_RIGHT_2 = 4;
-	public const int DRIVE_SHIFTER = 0;
+	const int DRIVE_LEFT_1 = 2;
+	const int DRIVE_LEFT_2 = 3;
+	const int DRIVE_RIGHT_1 = 5;
+	const int DRIVE_RIGHT_2 = 4;
+	const int DRIVE_SHIFTER = 0;
 
 	//Hanger Winch CAN ID
-	public const int HANGER_WINCH = 6;
+	const int HANGER_WINCH = 6;
 
 	//Hanger deploy (PCM 0)
-	public const int HANGER_DEPLOY_1 = 1;
-	public const int HANGER_DEPLOY_2 = 5;
+	const int HANGER_DEPLOY_1 = 1;
+	const int HANGER_DEPLOY_2 = 5;
 
 	//Intake CAN IDs
-	public const int INTAKE_ARM = 0;
-	public const int INTAKE_ROLLER = 7;
+	const int INTAKE_ARM = 0;
+	const int INTAKE_ROLLER = 7;
 
 	//Catapult solenoids (PCM 1)
-	public const int CAT_SHOOTER_1 = 0;
-	public const int CAT_SHOOTER_2 = 1;
-	public const int CAT_SHOOTER_3 = 2;
-	public const int CAT_SHOOTER_4 = 3;
+	const int CAT_SHOOTER_1 = 0;
+	const int CAT_SHOOTER_2 = 1;
+	const int CAT_SHOOTER_3 = 2;
+	const int CAT_SHOOTER_4 = 3;
 
 	//Catapult solenoids (PCM 0)
-	public const int CAT_SHORT_SHOT = 2;
-	public const int CAT_CLAMP = 3;
+	const int CAT_SHORT_SHOT = 2;
+	const int CAT_CLAMP = 3;
 
 	//Action Arm (PCM 0)
-	public const int ACTION_ARM = 4;
-	public const int ACTION_ARM_STOP = 6;
+	const int ACTION_ARM = 4;
+	const int ACTION_ARM_STOP = 6;
 
 	//Flashlight Relay
-	public const int FLASHLIGHT = 1;
+	const int FLASHLIGHT = 1;
 
 	//Camera Rightlight
-	public const int CAMERA_RING_LIGHT = 2;
+	const int CAMERA_RING_LIGHT = 2;
 
 	//Compressor Relay
-	public const int COMPRESSOR_RELAY = 0;
+	const int COMPRESSOR_RELAY = 0;
 
 
 	//sensors
-	public const int INTAKE_BEAM_BREAK = 1;
-	public const int DRIVETRAIN_ENCODER_LEFT_A = 3;
-	public const int DRIVETRAIN_ENCODER_LEFT_B = 4;
-	public const int DRIVETRAIN_ENCODER_RIGHT_A = 1;
-	public const int DRIVETRAIN_ENCODER_RIGHT_B = 2;
+	const int INTAKE_BEAM_BREAK = 1;
+	const int DRIVETRAIN_ENCODER_LEFT_A = 3;
+	const int DRIVETRAIN_ENCODER_LEFT_B = 4;
+	const int DRIVETRAIN_ENCODER_RIGHT_A = 1;
+	const int DRIVETRAIN_ENCODER_RIGHT_B = 2;
 
 	//SPARE_TALON = 1
 
