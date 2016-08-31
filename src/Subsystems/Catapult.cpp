72b50d195b05a9d54264e10999c589f911061870
James <jamestonthat@gmail.com>
2016-08-30 20:54:32 -0500

    Made changes to Catapult Files

--- src/Subsystems/Catapult.cpp
+++ src/Subsystems/Catapult.cpp
@@ -5,8 +5,29 @@
  *      Author: Zach Cody
  */
 
-#include "catapult.h"
+#include "Catapult.h"
+
+Catapult* Catapult::m_instance = 0; // Global Static pointer
 
 Catapult::Catapult() : Subsystem("Catapult") {
-	// TODO Auto-generated constructor stub
+	Catapult0 = new Solenoid(1,CAT_SHOOTER_1);
+	Catapult1 = new Solenoid(1,CAT_SHOOTER_2);
+	Catapult2 = new Solenoid(1,CAT_SHOOTER_3);
+	Catapult3 = new Solenoid(1,CAT_SHOOTER_4);
+}
+
+Catapult* Catapult::GetInstance() {
+	if(m_instance==0){
+		m_instance = new Catapult();
+		std::count << "info: Creating Catapult Class" << std:endl;
+	}
+	return m_instance;
+}
+
+void Catapult::SetState(bool on)
+{
+	Catapult0->Set(on);
+	Catapult1->Set(on);
+	Catapult2->Set(on);
+	Catapult3->Set(on);
 }
--- src/Subsystems/Catapult.h
+++ src/Subsystems/Catapult.h
@@ -8,13 +8,25 @@
 #define SRC_SUBSYSTEMS_CATAPULT_H_
 
 
-#include "Timer.h"
 #include "WPILib.h"
+#include "Constants/Ports.h"
 
 class Catapult : public Subsystem
 {
 public:
+	static Catapult* GetInstance();
+	void SetState(bool on);
+
+private:
 	Catapult();
+	static Catapult* m_instance;
+	Solenoid* Catapult0;
+	Solenoid* Catapult1;
+	Solenoid* Catapult2;
+	Solenoid* Catapult3;
+
+	bool m_on = 0;
+
 
 };
 
