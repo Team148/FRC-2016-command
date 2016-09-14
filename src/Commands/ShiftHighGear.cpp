ff8c4951d9145b100f4dce57e8fb438b1fd77aab
#beards <justin@bustedengineering.com>
2016-09-14 14:10:09 -0500

    made ShiftHighGear a one-shot command.

--- src/Commands/ShiftHighGear.cpp
+++ src/Commands/ShiftHighGear.cpp
@@ -8,25 +8,23 @@ ShiftHighGear::ShiftHighGear()
 // Called just before this Command runs the first time
 void ShiftHighGear::Initialize()
 {
-
+	Drivetrain::GetInstance()->SetGear(0);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void ShiftHighGear::Execute()
 {
-	Drivetrain::GetInstance()->SetGear(0);
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool ShiftHighGear::IsFinished()
 {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
 void ShiftHighGear::End()
 {
-
 }
 
 // Called when another command which requires one or more of the same
